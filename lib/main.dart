047578ad32961cf5aed2137989a5b55303e12ecd
bhuvaneshwaran <bhuvaneshwaran@bhuvaneshwarans-MacBook-Air.local>
2024-09-27 23:45:17 +0530

    list view setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:newproject/homescreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,74 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderRadius: const BorderRadius.all(Radius.circular(10)),
-      borderSide: const BorderSide(color: Colors.black, width: 2),
-    );
-    Widget buttonWidget({required String text}) {
-      return TextButton(
-        onPressed: () {},
-        style: TextButton.styleFrom(
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white,
-          minimumSize: const Size(100, 50),
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10))),
-        ),
-        child: Text(text),
-      );
-    }
-
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Todo Appliction",
-            style: TextStyle(
-                color: Colors.pink, fontWeight: FontWeight.bold, fontSize: 33),
-          ),
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Job",
-                    enabledBorder: border,
-                    focusedBorder: border,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: buttonWidget(text: "Insert"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: buttonWidget(text: "Update"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: buttonWidget(text: "Delete"),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: buttonWidget(text: "Clear"),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+        body: HomeScreen(),
       ),
     );
   }
